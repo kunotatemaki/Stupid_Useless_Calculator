8eab1798f9812ea4c8bd5796f2a87cbaafbee436
Raúl Feliz <raulfeliz@gmail.com>
2019-06-16 18:26:57 +0100

    add logic for enabling and disabling buttons

--- app/src/main/java/com/rukiasoft/stupiduselesscalculator/ui/main/MainFragment.kt
+++ app/src/main/java/com/rukiasoft/stupiduselesscalculator/ui/main/MainFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.rukiasoft.stupiduselesscalculator.R
+import com.rukiasoft.stupiduselesscalculator.databinding.MainFragmentBinding
 import com.rukiasoft.stupiduselesscalculator.ui.common.BaseFragment
 
 class MainFragment : BaseFragment() {
@@ -14,18 +16,69 @@ class MainFragment : BaseFragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+
+        binding.btnNumber1.setOnClickListener {
+            val text = "${binding.edtInput.text}1"
+            binding.edtInput.text = text
+            binding.btnNumber1.isEnabled = false
+            if (viewModel.isOperatorInserted(text)) {
+                binding.btnNumber0.isEnabled = true
+            } else {
+                binding.btnNumberMul.isEnabled = true
+            }
+        }
+
+        binding.btnNumber0.setOnClickListener {
+            val text = "${binding.edtInput.text}0"
+            binding.edtInput.text = text
+            if (viewModel.hasThreeZerosInserted(text)) {
+                binding.btnResult.isEnabled = true
+                binding.btnNumber0.isEnabled = false
+            }
+        }
+
+        binding.btnNumberMul.setOnClickListener {
+            val text = binding.edtInput.text.toString()
+            val newText = if (viewModel.checkIfOperatorIsInserted(text)) {
+                viewModel.replaceLastChar(text, MainViewModel.Operator.MULTIPLY.op)
+            } else {
+                viewModel.appendChar(text, MainViewModel.Operator.MULTIPLY.op)
+            }
+            binding.edtInput.text = newText
+            binding.btnNumberMul.isEnabled = false
+            binding.btnNumber1.isEnabled = true
+        }
+
+        binding.btnDelete.setOnClickListener {
+            val text = binding.edtInput.text.toString()
+            val newText = viewModel.removeLastCharacter(text)
+            binding.edtInput.text = newText
+            binding.btnNumberMul.isEnabled = false
+            binding.btnNumber1.isEnabled = false
+            binding.btnNumber0.isEnabled = false
+            binding.btnResult.isEnabled = false
+            when(viewModel.getButtonToActivate(newText)){
+                MainViewModel.ActiveButton.UNO -> binding.btnNumber1.isEnabled = true
+                MainViewModel.ActiveButton.MULTIPLY -> binding.btnNumberMul.isEnabled = true
+                MainViewModel.ActiveButton.ZERO -> binding.btnNumber0.isEnabled = true
+            }
+        }
+
 
     }
 
--- app/src/main/java/com/rukiasoft/stupiduselesscalculator/ui/main/MainViewModel.kt
+++ app/src/main/java/com/rukiasoft/stupiduselesscalculator/ui/main/MainViewModel.kt
@@ -3,6 +3,87 @@ package com.rukiasoft.stupiduselesscalculator.ui.main
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(): ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    enum class Operator(val op: Char) {
+        ADD('+'),
+        MINUS('-'),
+        MULTIPLY('*'),
+        DIVIDER('/');
+
+        companion object {
+            fun getOperationFromChar(op: Char?): Operator? {
+                return when (op) {
+                    ADD.op -> ADD
+                    MINUS.op -> MINUS
+                    MULTIPLY.op -> MULTIPLY
+                    DIVIDER.op -> DIVIDER
+                    else -> null
+                }
+            }
+        }
+    }
+
+    enum class ActiveButton {
+        UNO, MULTIPLY, ZERO
+    }
+
+    var first: Double? = null
+    var second: Double? = null
+    var operation: Operator? = null
+
+    fun performOperation(): Double? {
+        operation?.let {
+            val fOp = first ?: return null
+            val sOp = second ?: return null
+            when (it) {
+                Operator.ADD -> fOp.plus(sOp)
+                Operator.MINUS -> fOp.minus(sOp)
+                Operator.MULTIPLY -> fOp.times(sOp)
+                Operator.DIVIDER -> fOp.div(sOp)
+            }
+        }
+        return null
+    }
+
+    fun checkIfOperatorIsInserted(text: String?): Boolean {
+        val candidateCharacter = text?.getOrNull(text.lastIndex)
+        Operator.getOperationFromChar(candidateCharacter)?.let {
+            return true
+        }
+        return false
+    }
+
+    fun replaceLastChar(text: String, char: Char): String {
+        var removed = text.removeRange(text.lastIndex, text.lastIndex + 1)
+        removed = removed.plus(char)
+        return removed
+    }
+
+    fun appendChar(text: String, char: Char): String =
+        text.plus(char)
+
+    fun isOperatorInserted(text: String): Boolean =
+        null != Operator.getOperationFromChar(text.getOrNull(text.lastIndex - 1))
+
+    fun hasThreeZerosInserted(text: String): Boolean =
+        "000" == text.substring(text.lastIndex - 2)
+
+    fun removeLastCharacter(text: String): String =
+        if (text.isNotBlank()) {
+            text.removeRange(text.lastIndex, text.lastIndex + 1)
+        } else {
+            text
+        }
+
+    fun getButtonToActivate(text: String): ActiveButton {
+        if (text.isBlank()) return ActiveButton.UNO
+        val char = text.getOrNull(text.lastIndex)
+        return if (char == '0') ActiveButton.ZERO
+        else if(char == '1' && text.length == 1) ActiveButton.MULTIPLY
+        else if(char == '1') ActiveButton.ZERO
+        else if (Operator.getOperationFromChar(char) != null) ActiveButton.UNO
+        else ActiveButton.UNO
+    }
 }
+
+
