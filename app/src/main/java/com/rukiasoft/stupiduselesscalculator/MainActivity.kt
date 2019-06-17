054d829008980b4130c0c122b9e92996ba7755e5
Raúl Feliz <raulfeliz@gmail.com>
2019-06-17 23:30:16 +0100

    add ads

--- app/src/main/java/com/rukiasoft/stupiduselesscalculator/MainActivity.kt
+++ app/src/main/java/com/rukiasoft/stupiduselesscalculator/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.rukiasoft.stupiduselesscalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.rukiasoft.stupiduselesscalculator.databinding.MainActivityBinding
 import com.rukiasoft.stupiduselesscalculator.ui.main.MainFragment
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.view.*
 
 class MainActivity : DaggerAppCompatActivity() {
 
+    lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        val adRequest = AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .build()
+        binding.adView.loadAd(adRequest)
     }
 
 }
